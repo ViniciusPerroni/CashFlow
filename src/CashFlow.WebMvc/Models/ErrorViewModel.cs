a21c3f96314071873dfdaeca7608c3806628e5f0
Vinicius Perroni <viniciusnp3@gmail.com>
2025-02-25 03:52:46 -0300

    Remoção projeto não utilizado

--- src/CashFlow.WebMvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CashFlow.WebMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
